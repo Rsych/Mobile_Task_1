bbdd7b0254c004ac1d4b5f7dd2304a349c5c9cf6
Ryan J. W. Kim <tamielryan@gmail.com>
2022-11-12 07:00:08 +0200

    DesignableUITextField

---
+++ Mobile_Task_1/DesignableUITextField.swift
@@ -0,0 +1,75 @@
+//
+//  DesignableUITextField.swift
+//  Mobile_Task_1
+//
+//  Created by Ryan J. W. Kim on 2022/11/12.
+//
+
+import UIKit
+
+@IBDesignable
+class DesignableUITextField: UITextField {
+    
+    // Provides left padding for images
+    override func leftViewRect(forBounds bounds: CGRect) -> CGRect {
+        var textRect = super.leftViewRect(forBounds: bounds)
+        textRect.origin.x += leftPadding
+        return textRect
+    }
+
+    override func rightViewRect(forBounds bounds: CGRect) -> CGRect {
+        var textRect = super.rightViewRect(forBounds: bounds)
+        textRect.origin.x += rightPadding
+        return textRect
+    }
+    
+    @IBInspectable var leftImage: UIImage? {
+        didSet {
+            updateView()
+        }
+    }
+
+    @IBInspectable var rightText: String? {
+        didSet {
+            updateView()
+        }
+    }
+    
+    @IBInspectable var leftPadding: CGFloat = 0
+    @IBInspectable var rightPadding: CGFloat = 0
+    
+    @IBInspectable var color: UIColor = UIColor.lightGray {
+        didSet {
+            updateView()
+        }
+    }
+    
+    func updateView() {
+        if let image = leftImage, let text = rightText {
+            leftViewMode = UITextField.ViewMode.always
+            rightViewMode = UITextField.ViewMode.always
+
+            let imageView = UIImageView(frame: CGRect(x: 0, y: 0, width: 20, height: 20))
+            imageView.contentMode = .scaleAspectFit
+            imageView.image = image
+            // Note: In order for your image to use the tint color, you have to select the image in the Assets.xcassets and change the "Render As" property to "Template Image".
+            imageView.tintColor = color
+            leftView = imageView
+
+            let rightText = UILabel(frame: CGRect(x: 0, y: 0, width: 27, height: 18))
+            rightText.contentMode = .scaleAspectFit
+            rightText.text = text
+            rightText.textColor = color
+            rightView = rightText
+        } else {
+            leftViewMode = UITextField.ViewMode.never
+            rightViewMode = UITextField.ViewMode.never
+            leftView = nil
+            rightView = nil
+        }
+        
+        // Placeholder text color
+        attributedPlaceholder = NSAttributedString(string: placeholder != nil ?  placeholder! : "", attributes:[NSAttributedString.Key.foregroundColor: color])
+    }
+}
+
