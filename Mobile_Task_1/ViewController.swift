78274d45f89c4cc608ec288388691f60940d4547
Ryan J. W. Kim <tamielryan@gmail.com>
2022-11-12 05:42:45 +0200

    Initial Commit

---
+++ Mobile_Task_1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Mobile_Task_1
+//
+//  Created by Ryan J. W. Kim on 2022/11/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
