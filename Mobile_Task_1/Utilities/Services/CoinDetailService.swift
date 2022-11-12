11ab0c61adb270c1b27125845afe71062931654c
Ryan J. W. Kim <tamielryan@gmail.com>
2022-11-12 10:29:38 +0200

    Service_Init

---
+++ Mobile_Task_1/Utilities/Services/CoinDetailService.swift
@@ -0,0 +1,32 @@
+//
+//  CoinDetailService.swift
+//  Mobile_Task_1
+//
+//  Created by Ryan J. W. Kim on 2022/11/12.
+//
+
+import Foundation
+import Combine
+
+class CoinDetailService {
+    @Published var coinDetails: CoinDetail? = nil
+    
+    var coinDetailSubscription: AnyCancellable?
+    
+    
+    init(coin: Coin) {
+        self.coin = coin
+        getCoinDetails()
+    }
+    func getCoinDetails() {
+        guard let url = URL(string: OptionsConfig.Net.baseUrl + coin.id + "/USD") else { return }
+        
+        subscription = NetworkManager.download(url: url)
+            .decode(type: CoinDetail.self, decoder: JSONDecoder())
+            .receive(on: DispatchQueue.main)
+            .sink(receiveCompletion: NetworkManager.handleCompletion, receiveValue: { [weak self] (returnedCoinDetails) in
+                self?.coinDetails = returnedCoinDetails
+                self?.coinDetailSubscription?.cancel()
+            })
+    }
+}
